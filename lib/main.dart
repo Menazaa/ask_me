32b745011b9bd71f1e48181799b3f0070b33523b
MohamedMenaz3 <myyli1999@gmail.com>
2021-02-04 13:50:08 +0200

    intail
    intial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Center(
+              child: Text('Ask me'),
+            ),
+            backgroundColor: Colors.blueAccent,
+          ),
+          body: AskMePage(),
+        ),
       ),
     );
+
+class AskMePage extends StatefulWidget {
+  @override
+  _AskMePageState createState() => _AskMePageState();
+}
+
+class _AskMePageState extends State<AskMePage> {
+  int ballNumber = 1;
+  void changeTheAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: changeTheAnswer,
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
